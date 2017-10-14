f711f8542f37fe6e6ab747d137d7f3b82cbadf87
Gabriel Brito <gabriel.brito55@gmail.com>
2017-10-14 01:16:53 -0300

    contabilizacao dos exemplos

--- src/decision_tree/Tree.java
+++ src/decision_tree/Tree.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public class Tree {
 	final static int N_ATTR = 4;
+	final static int COUNTER_SIZE = 30;
 	
 	//Atributos
 	final static int GENDER = 0;
@@ -13,16 +14,52 @@ public class Tree {
 	
 	
 	private ArrayList<int[]>examples;
+	ArrayList<int[]> counters;
 	
 	public Tree(ArrayList<int[]> examples){
 		this.examples = examples;
+		counters = accountExamples();
+		
 	}
 	
 	public void build(){
+		
 		int best = chooseBestAttr();
 	}
 	
-	public int chooseBestAttr(){
+	private ArrayList<int[]> accountExamples(){
+		//Cada um dos elementos do arraylist eh um vetor
+		//Cada cada campo dos vetores guarda a quantidade de 
+		//occorencias de um determinado tipo de atributo
+		
+		for(int i = 0; i < N_ATTR; i++){
+			counters.add(new int[COUNTER_SIZE]);
+		}
+		
+		//Percorrendo os exemplos
+		int[] example, counter;
+		for(int i = 0; i < examples.size(); i++){
+			example = examples.get(i);
+			//Contabilizando genero
+			counter = counters.get(GENDER);
+			counter[example[GENDER]]++;
+			
+			//Faixa etaria
+			counter = counters.get(AGE);
+			counter[example[AGE]]++;
+			
+			//Emprego
+			counter = counters.get(OCCUPATION);
+			counter[example[OCCUPATION]]++;
+			
+			//Avaliacao
+			counter = counters.get(STARS);
+			counter[example[STARS]]++;
+		}
+		return counters;
+	}
+	
+	private int chooseBestAttr(){
 		
 	}
 }
